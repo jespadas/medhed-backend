06ec83c9dd87e8bef7a0e592d09d923604f7ed21
Julio Espadas <contact@julioespadas.com>
2022-01-24 18:46:52 +0100

    refactoting

--- medhed-backend/src/main/java/com/jespadas/medhedBackendApi/services/CancelReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.jespadas.medhedBackendApi.services;
-
-import com.jespadas.medhedBackendApi.exceptions.ReservationException;
-
-public interface CancelReservationService {
-
-	public String deleteReservation(String locator) throws ReservationException;
-
-}
